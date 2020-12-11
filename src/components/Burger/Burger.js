c413a88c426affcbbd3a2c8dda9bce2c95d1875d
Crystal1831 <43097573+Crystak1831@users.noreply.github.com>
2020-12-10 23:10:49 -0500

    add folders

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,12 +7,17 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 //start the ingredient render burger to screen
 
 const burger = (props) =>{
+
+    //Object.keys returns an array of a given object's own property names
     const transformedIngrediencts = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
                return <BurgerIngredient key = {igKey + i} type={igKey}/>
             })
-        });
+        })
+        .reduce((arr,el) =>{
+            return  arr.concat(el)
+        },[])
     console.log(transformedIngrediencts)
 
     return(
