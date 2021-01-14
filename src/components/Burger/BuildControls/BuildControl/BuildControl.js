b16abf579bbd4659f42c3075bfba6b30f54d562b
Crystal1831 <43097573+Crystak1831@users.noreply.github.com>
2021-01-14 14:01:51 -0500

    hello

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import classes from './BuildControl.css'
+
 
 const buildControl = (props) =>{
     return <div className={classes.BuildControl}>
--- src/components/Burger/BuildControls/BurgerControls.js
+++ src/components/Burger/BuildControls/BurgerControls.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Salad', type: 'salad'}
+
+]
+
 const buildControls = (props) =>{
     return <div>
-
+        <button></button>
     </div>
 
 }
